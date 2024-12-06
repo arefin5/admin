503dba455b20b988c87258ec2de73512f9106993
arefin5 <arefintalukder5@gmail.com>
2024-12-07 02:26:52 +0600

    chat implement

--- app/admin/order-history/page.jsx
+++ app/admin/order-history/page.jsx
@@ -7,86 +7,7 @@ import ChatComponentHooks from "@/components/chatHook";
 import Image from "next/image"
 import { Send } from "lucide-react"
 
-// const socket = io("http://145.223.22.239:5001", { autoConnect: false });
-// // const socket = io("http://localhost:5001", { autoConnect: false });
-
-// // Page Component
-// export default function Page() {
-//     const [showChatBox, setShowChatBox] = useState(false);
-//     const [conversations, setConversations] = useState([]);
-//     const [selectedUser, setSelectedUser] = useState(null); // Current conversation user
-//     const [user, setUser] = useState(null); // Actual authenticated user
-//     const [token, setToken] = useState(null);
-//     const [loadingMessages, setLoadingMessages] = useState(false); // Loading state
-//     const [newMessage, setNewMessage] = useState("");
-//     useEffect(() => {
-//         const tokenFromStorage = localStorage.getItem("token");
-//         setToken(tokenFromStorage);
-//     }, []);
-
-//     useEffect(() => {
-//         if (!token) return;
-
-//         socket.connect();
-//         socket.emit("authenticate", token, (response) => {
-//             if (response.status === "success") {
-//                 setUser(response.user._id);
-//                 socket.emit("join", response.user._id);
-//                 fetchConversations(response.user._id);
-//             } else {
-//                 console.error(response.message);
-//             }
-//         });
-
-//         socket.on("receiveMessage", (message) => {
-//             if (selectedUser && message.sender === selectedUser.id) {
-//                 setMessages((prevMessages) => [...prevMessages, message]);
-//             }
-//         });
-
-//         return () => {
-//             socket.disconnect();
-//         };
-//     }, [token, selectedUser]);
-
-//     const fetchConversations = (userId) => {
-//         socket.emit("getConversations", userId, (response) => {
-//             if (response.status === "success") {
-//                 setConversations(response.conversations);
-//                 setShowChatBox(true);
-//             } else {
-//                 console.error(response.message);
-//             }
-//         });
-//     };
-
-//     const selectConversation = (contact) => {
-//         setSelectedUser(contact);
-//         setLoadingMessages(true); // Loading state
-//     };
-//     function getUsernameFromEmail(email) {
-//         if (!email.includes("@")) {
-//             throw new Error("Invalid email address");
-//         }
-//         return email.split("@")[0];
-//     }
-//     const handleSendMessage = () => {
-//         const sender = user;
-//         const receiver = selectedUser.userId;
-//         const data = { sender, receiver, message: newMessage };
-
-//         socket.emit("sendMessage", data, (response) => {
-//             if (response.status === "Message delivered") {
-//                 console.log("Message sent successfully!");
-//             } else {
-//                 console.error("Message delivery failed:", response.message);
-//             }
-//         });
-
-//         setNewMessage(""); 
-//     };
-
-const socket = io("https://backend.bedbd.com", { autoConnect: false });
+const socket = io("http://145.223.22.239:5001", { autoConnect: false });
 // const socket = io("http://localhost:5001", { autoConnect: false });
 
 // Page Component
@@ -164,6 +85,8 @@ export default function Page() {
 
         setNewMessage(""); 
     };
+
+
 return(<>
     <div className='w-full h-full p-8 flex gap-8 '>
         {/* Left side */}
